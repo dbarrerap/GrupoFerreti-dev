438e0ccd290fd0ac5a00c887fec3d9f895c3d17b
David E. Barrera <dbarrerap@live.com>
2022-01-12 17:45:23 -0500

    Candidatos Migration

---
+++ database/migrations/2022_01_12_223949_create_candidatos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidatos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('cedula');
+            $table->string('nombre_completo');
+            $table->date('fecha_nacimiento');
+            $table->string('numero_movil');
+            $table->string('numero_casa');
+            $table->string('email');
+            $table->enum('estado_civil', ['soltero/a', 'casado/a', 'divorciado/a', 'viudo/a']);
+            $table->enum('sexo', ['hombre', 'mujer', 'no binario']);
+            $table->text('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidatos');
+    }
+}
